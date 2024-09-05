e331d95ce695aac9f816f3cf3f3021ca4a4464d5
Sana Gaikwad <gaikwadsana@gmail.com>
2024-09-05 18:20:03 +1000

    feat: add click event on button

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -2,12 +2,17 @@ import { Button, ButtonProps } from "@chakra-ui/react";
 
 interface ButtonComponentProps extends ButtonProps {
   label: string;
+  onClick: () => void;
 }
 
-export const ButtonComponent = ({ label, ...props }: ButtonComponentProps) => {
+export const ButtonComponent = ({
+  label,
+  onClick,
+  ...props
+}: ButtonComponentProps) => {
   return (
     <>
-      <Button {...props} colorScheme="teal">
+      <Button {...props} colorScheme="teal" onClick={onClick}>
         {" "}
         {label}
       </Button>
