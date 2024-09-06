732e93c61f1dfd27d3d7a175904e4d6bb8a8bbe6
Sana Gaikwad <gaikwadsana@gmail.com>
2024-09-06 19:48:14 +1000

    chore: code cleanup and remove unwanted files

--- components/heading/Heading.tsx
+++ components/heading/Heading.tsx
@@ -2,9 +2,5 @@ import { Heading as ChakraHeading } from "@chakra-ui/react";
 import { HeadingProps } from "./Heading.types";
 
 export const Heading = ({ children, ...props }: HeadingProps) => {
-  return (
-    <>
-      <ChakraHeading {...props}>{children}</ChakraHeading>
-    </>
-  );
+  return <ChakraHeading {...props}>{children}</ChakraHeading>;
 };
